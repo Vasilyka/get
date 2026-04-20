406688c99342c1e371a5c1e64e4d1b4ec4fa0d82
Калачиков Василий <kalachikov.va@phystech.edu>
2026-04-20 09:49:37 +0300

    первая попытка 20 апреля

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -5,10 +5,10 @@ leds = [16,20,21,25,26,17,27,22]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 
-dynamic_range=3.3
+dynamic_range = 3.3
 def voltage_to_number(voltage):
     if not(0.0 <= voltage <= dynamic_range):
-        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dinamic_range:.2f} В")
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В")
         print("Устанавливаем 0.0 В")
         return 0
     return int(voltage*255//dynamic_range)
