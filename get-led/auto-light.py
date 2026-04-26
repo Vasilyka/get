1245b305e897bb6f810ea746faac8c7951f05d45
Василий Калачиков <kalachikov.va@phystech.edu>
2026-04-26 20:09:06 +0300

    Ladno

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -3,10 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 led = 26
+button = 13
+photor = 6
+GPIO.setup(photor, GPIO.IN)
 GPIO.setup(led, GPIO.OUT)
 state = 0
-period = 1.0
+period = 1
 while True:
+    state = not(GPIO.input(photor))
     GPIO.output(led, state)
-    state = not state
-    time.sleep(period/2)
+    time.sleep(period)
