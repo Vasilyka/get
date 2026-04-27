4a4ac74535aefee0911e96e5e85cc00e38b11907
Василий Калачиков <kalachikov.va@phystech.edu>
2026-04-27 11:28:23 +0300

    пупупу

---
+++ get-dac/r2r-sin.py
@@ -0,0 +1,23 @@
+import r2r_dac as r2r
+import signal_gen as sg
+import time
+import RPi.GPIO as GPIO
+
+amp = 3.2
+sif = 10
+saf = 1000
+leds = [16,20,21,25,26,17,27,22]
+dynamic_range=3.14
+start = float(time.time())
+
+if __name__ == '__main__':
+    try:
+        dac = r2r.R2R_DAC(leds, dynamic_range, True)
+        while True:
+            try:
+                dac.set_voltage((sg.get_sin_wave_amplitude(sif, 2)/2)*amp)
+                time.sleep(1/saf)
+            except ValueError:
+                print('aaaa')
+    finally:
+        dac.deinit()
---
+++ get-dac/signal_generator.py
@@ -0,0 +1,11 @@
+import numpy
+import time
+import math
+
+start = float(time.time())
+
+def get_sin_wave_amplitude(freq, t):
+    sin_val = math.sin(2*math.pi*freq*(float(time.time())-start))+1
+    return sin_val
+def wait(f):
+    time.sleep(1/f)
