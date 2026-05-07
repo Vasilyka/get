4f5f1796e5ea21c624b53014beaa45a69cc6816c
Василий Калачиков <Kalachikov.va@phystech.edu>
2026-05-07 18:29:13 +0300

    Update папапа.py

--- get-dac/папапа.py
+++ get-dac/папапа.py
@@ -1,36 +1,7 @@
-import RPi.GPIO as GPIO
-class PWM_DAC:
-    def __init__(self,gpio_pin,pwm_frequency,dynamic_range,verbose=False):
-        self.gpio_pin=gpio_pin
-        self.dynamic_range=dynamic_range
-        self.verbose=verbose
-        self.pwm_frequency=pwm_frequency
+Для работы с I2C после каждой перезагрузки Raspberry Pi необходимо выполнить две команды в терминале:
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_pin,GPIO.OUT)
-        self.pwm=GPIO.PWM(self.gpio_pin,self.pwm_frequency)
-        self.pwm.start(0.0)
-    def set_number(self,number):
-       GPIO.output(self.gpio_pin,[int(element) for element in bin(number)[2:].zfill(8)])
-    def set_voltage(self,voltage):
-       if not (0.0<=voltage<=self.dynamic_range):
-           print(f"Напряжение выходит за динамический диапазон ЦАП (0.00- {self.dynamic_range:.2f} В)")
-           print("Устанавливаем 0.0 В")
-           return self.pwm.ChangeDutyCycle (0)
-       duty=int(voltage/self.dynamic_range*100)
-       self.pwm.ChangeDutyCycle(duty)
-    def deinit(self):
-        GPIO.output(self.gpio_pin,0)
-        GPIO.cleanup()
-if  __name__=="__main__":
-    try:
-        dac = PWM_DAC(12,1000,3.183, True)
+raspi-gpio set 2 a0
+raspi-gpio set 3 a0
+После выставления пинам GPIO2 и GPIO3 первой альтернативной функции (функции ножек линий SDA и SCL устройства i2c1) стоит убедиться в корректности выставления этих функций командой:
 
-        while True:
-            try:
-                voltage=float(input("Введите напряжение в вольтах:"))
-                dac.set_voltage(voltage)
-            except ValueError:
-               print("Вы ввели не число.Попробуйте ещё раз\n")
-    finally:
-        dac.deinit()
+raspi-gpio get
