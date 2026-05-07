c3f863b879ee51ea8e9510995fb677caef2594a8
Василий Калачиков <Kalachikov.va@phystech.edu>
2026-05-07 18:33:11 +0300

    Update папапа.py

--- get-dac/папапа.py
+++ get-dac/папапа.py
@@ -1,7 +1,46 @@
-Для работы с I2C после каждой перезагрузки Raspberry Pi необходимо выполнить две команды в терминале:
+import smbus
 
-raspi-gpio set 2 a0
-raspi-gpio set 3 a0
-После выставления пинам GPIO2 и GPIO3 первой альтернативной функции (функции ножек линий SDA и SCL устройства i2c1) стоит убедиться в корректности выставления этих функций командой:
+class MCP4725:
+    def __init__(self, dynamic_range, address=0x61, verbose=True):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
 
-raspi-gpio get
+    def deinit(self):
+        self.bus.close()
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("Dai celoye chislo")
+        elif not (0<=number<=4095):
+            print('Out of range (12 bit)')
+        else:
+            first_byte = self.wm|self.pds|number>>8
+            second_byte = number & 0xFF
+            self.bus.write_byte_data(0x61, first_byte, second_byte)
+
+            if self.verbose:
+                print(f'Chislo: {number}, otpr po I2C: [0x{(self.address<<1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n')
+    
+    def set_voltage(self, voltage):
+        if not(0.0<=voltage<=self.dynamic_range):
+            print('Напряжение за рамками диапазона, сброс до 0.0 В')
+        else:
+            val = int(voltage/self.dynamic_range*4095)
+            self.set_number(val)
+
+if __name__ == "__main__":
+    try:
+        dac = MCP4725(5, 0x61, True)
+        while True:
+            try:
+                voltage = float(input('Введите напряжение: '))
+                dac.set_voltage(voltage)
+            except ValueError:
+                print('Вы ввели не число...зачем...')
+            except KeyboardInterrupt:
+                print('Thank you for using RaspberryPie, bye!')
+    finally:
+        dac.deinit()
