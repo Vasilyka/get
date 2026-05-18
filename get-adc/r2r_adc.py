a72ac403b55a6a24a046af505ebe3741b5987566
Калачиков Василий <kalachikov.va@phystech.edu>
2026-05-18 10:02:50 +0300

    ;

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -4,7 +4,7 @@ def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
 class R2R_ADC:
-    def __init__(self, dynamic_range, compare_time = 0.001, verbose = False):
+    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
         self.dynamic_range = dynamic_range
         self.verbose = verbose
         self.compare_time = compare_time
@@ -44,7 +44,7 @@ class R2R_ADC:
 if __name__ == "__main__":
     dac = None
     try:
-        dac = R2R_ADC(3.23)
+        dac = R2R_ADC(3.295)
         while True:
             print(dac.get_sc_voltage())
     finally:
