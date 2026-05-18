4928fbdc303fb79cf0e6b6f0a45e8924390d288a
Калачиков Василий <kalachikov.va@phystech.edu>
2026-05-18 11:05:52 +0300

    rg

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -34,7 +34,7 @@ class MCP4725_DAC:
 
 if __name__ == "__main__":
     try:
-        dac = MCP4725_DAC(5.3)
+        dac = MCP4725_DAC(4.215)
         while True:
             try:
                 voltage = float(input("Введите напряжение в Вольтах: "))
