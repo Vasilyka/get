cb95fd91d78f3826c644d6dbb838ecb20c947580
Василий Калачиков <kalachikov.va@phystech.edu>
2026-05-18 11:26:22 +0300

    f

--- get-dac/pwm_tri.py
+++ get-dac/pwm_tri.py
@@ -1,54 +1,36 @@
+import r2r_dac as r2r
+import signal_generator as sg
 import time
 import RPi.GPIO as GPIO
+import smbus
+amp = 3.2
+sif = 10
+saf = 1000
+leds = [16,20,21,25,26,17,27,22]
+dynamic_range=3.14
+start = float(time.time())
 
-PWM_PIN = 18
-PWM_FREQ = 1000
-AMP = 3.0          # Амплитуда (В)
-FREQ = 2           # Частота треугольника (Гц)
-SAMPLING_FREQ = 200
+class Triag:
+    def __init__(self, led=12, F=200, v=1, f=0.05):
+        GPIO.setmode(GPIO.BCM)
+        self.led=led
+        self.step=v
+        self.tim=1/f
+        self.f=F
+        GPIO.setup(led, GPIO.OUT)
+        pwm=GPIO.PWM(self.led, self.f)
+        duty=0.0
+        pwm.start(duty)
+        while True:
+            pwm.ChangeDutyCycle(duty)
+            time.sleep(self.step)
+            duty+=self.step
+            if duty>=100.0 or duty<=0.0:
+                self.step=-self.step
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PWM_PIN, GPIO.OUT)
-pwm = GPIO.PWM(PWM_PIN, PWM_FREQ)
-pwm.start(0)
-
-start_time = time.time()
-
-def get_triangle():
-    period = 1.0 / FREQ
-    t_mod = (time.time() - start_time) % period
-    phase = t_mod / period
-    
-    if phase < 0.5:
-        return phase * 2      # 0 → 1
-    else:
-        return (1 - phase) * 2 # 1 → 0
-
-def set_voltage_pwm(voltage):
-    """Устанавливает напряжение на выходе ШИМ (с ФНЧ)"""
-    max_v = 3.3
-    if voltage > max_v:
-        voltage = max_v
-    if voltage < 0:
-        voltage = 0
-    duty = (voltage / max_v) * 100
-    pwm.ChangeDutyCycle(duty)
-
-def wait_for_sampling():
-    time.sleep(1.0 / SAMPLING_FREQ)
 
 if __name__ == '__main__':
     try:
-        print(f"Треугольник {FREQ} Гц, амплитуда {AMP} В")
-        while True:
-            tri_val = get_triangle()
-            voltage = tri_val * AMP
-            set_voltage_pwm(voltage)
-            # Для отладки (раскомментируйте):
-            # print(f"{tri_val:.3f} -> {voltage:.2f} В")
-            wait_for_sampling()
-    except KeyboardInterrupt:
-        print("\nСтоп")
+        dac = Triag(F=500, f=50)
     finally:
-        pwm.stop()
-        GPIO.cleanup()
+        dac.deinit()"""
